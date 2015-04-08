0367cfd6b18ca61697028a5d6e12358d7b0d69fc
Aleksander Sadecki <aleksander.sadecki@schibsted.pl>
2015-04-08 11:54:09 +0200

    Added endpoint for getting hands of one player

--- src/main/java/bridge/resources/TableResource.java
+++ src/main/java/bridge/resources/TableResource.java
@@ -1,16 +1,19 @@
 package bridge.resources;
 
 import bridge.domain.Deck;
+import bridge.domain.Hand;
+import bridge.domain.PlayerPlace;
 import bridge.domain.Table;
 import bridge.shuffler.ShufflerService;
 import com.codahale.metrics.annotation.Timed;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/table")
+@Path("/hand")
 @Produces(MediaType.APPLICATION_JSON)
 public class TableResource {
 
@@ -24,7 +27,19 @@ public class TableResource {
 
     @GET
     @Timed
-    public Table table() {
+    public Table getTable() {
         return shufflerService.shuffle(deck);
+    }
+
+    @GET
+    @Timed
+    @Path("/{player}")
+    public Hand getHand(@PathParam("player") String playerName) {
+        return shufflerService.shuffle(deck).getPlayers()
+            .stream()
+            .filter(player -> player.getName().equals(PlayerPlace.valueOf(playerName)))
+            .findFirst()
+            .get()
+            .getHand();
     }
 }
