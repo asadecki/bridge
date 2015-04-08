48e001f7b39370232f50a749cbb39afc18f78dab
Aleksander Sadecki <aleksander.sadecki@schibsted.pl>
2015-04-08 15:40:28 +0200

    Pictures of cards added but do now work

--- front/public/scripts/card.js
+++ front/public/scripts/card.js
@@ -1,11 +1,44 @@
 var Card = React.createClass({
     render: function () {
         return (
-            <div className="card">
-                <h2 className="cardAuthor"> {this.props.value} </h2>
-                <h2> {this.props.color} </h2>
+            <div className='card'>
+                <img src={'images/ck.gif'}/>
             </div>
             );
+    },
+
+    valueShortcut: function (value) {
+        if (value === 'TWO') {
+            return 2;
+        } else if (value === 'THREE') {
+            return 3;
+        } else if (value === 'FOUR') {
+            return 4;
+        } else if (value === 'FIVE') {
+            return 5;
+        } else if (value === 'SIX') {
+            return 6;
+        } else if (value === 'SEVEN') {
+            return 7;
+        } else if (value === 'EIGHT') {
+            return 8;
+        } else if (value === 'NINE') {
+            return 9;
+        } else if (value === 'TEN') {
+            return 10;
+        } else if (value === 'JACK') {
+            return 'j';
+        } else if (value === 'QUEEN') {
+            return 'q';
+        } else if (value === 'KING') {
+            return 'k';
+        } else if (value === 'ACE') {
+            return 1;
+        }
+    },
+
+    colorShortcut: function (color) {
+        return color.charAt(0);
     }
 });
 
@@ -31,7 +64,7 @@ var CardBox = React.createClass({
     },
     render: function () {
         return (
-            <div className="commentBox">
+            <div>
                 <h1>Cards</h1>
                 <CardList data={this.state.data} />
             </div>
@@ -44,11 +77,11 @@ var CardList = React.createClass({
         var cardNodes = this.props.data.map(function (card, index) {
             return ( <Card value={card.value} color={card.color} key={index}> xxx</Card> );
         });
-        return ( <div className="cardList"> {cardNodes} </div> );
+        return ( <div className='cardList'> {cardNodes} </div> );
     }
 });
 
 React.render(
-    <CardBox url="cards.json" pollInterval={2000} />,
+    <CardBox url='cards.json' pollInterval={2000} />,
     document.getElementById('content')
 );
